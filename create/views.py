29c1d799fe4175090165c28fc8634cb8c4cd1be9
Spyler <alkatalankar@gmail.com>
2020-04-26 22:00:32 +0530

    version 1.1

--- create/views.py
+++ create/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def hello(request):
+	return HttpResponse("Hello World!")
 # Create your views here.
+
+def About_me(request):
+	return render(request, 'main.html')
